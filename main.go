a9bc46fa71f3f89b2b433bf6de226f934346752c
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-06-02 08:16:39 +0900

    file path

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"os/user"
-	"log"
+    "log"
+    "os"
+    "net/http"
+    "path"
+    "path/filepath"
+    "io"
 )
 
 func main() {
-	u, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
+    cwd, err := os.Getwd()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if ok, err := path.Match("/data/*.txt", r.URL.Path); err != nil || !ok {
+            http.NotFound(w, r)
+            return
+        }
+
+        name := filepath.Join(cwd, "data", filepath.Base(r.URL.Path))
+        f, err := os.Open(name)
+        if err != nil {
+            http.NotFound(w, r)
+            return
+        }
+        defer f.Close()
+        io.Copy(w, f)
+    })
+    http.ListenAndServe(":8080", nil)
 }
